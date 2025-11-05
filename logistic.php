63ae3792b2248b4a8eafe4e54d4d4d64adf5e0b4
Adarsh morey <moreyadarsh63@gmail.com>
2025-11-05 17:55:01 +0530

    title change

--- logistic.php
+++ logistic.php
@@ -6,7 +6,8 @@
 
 
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <title> Admin Dashboard</title>
+  <title> Admin Dash</title>
+
   
   <meta
     content="width=device-width, initial-scale=1.0, shrink-to-fit=no"
